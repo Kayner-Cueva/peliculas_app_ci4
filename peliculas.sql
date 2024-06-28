2b4f29540755b61abd42d9aab6e53ebf00285297
Kayner-Cueva <76232385+Kayner-Cueva@users.noreply.github.com>
2024-06-28 12:13:18 -0500

    Add files via upload

---
+++ peliculas.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-06-2024 a las 19:13:03
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `peliculas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `auditoria`
+--
+-- Error leyendo la estructura de la tabla peliculas.auditoria: #1932 - Table &#039;peliculas.auditoria&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.auditoria: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`auditoria`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `autores`
+--
+-- Error leyendo la estructura de la tabla peliculas.autores: #1932 - Table &#039;peliculas.autores&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.autores: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`autores`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+-- Error leyendo la estructura de la tabla peliculas.categoria: #1932 - Table &#039;peliculas.categoria&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.categoria: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`categoria`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `episodios`
+--
+-- Error leyendo la estructura de la tabla peliculas.episodios: #1932 - Table &#039;peliculas.episodios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.episodios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`episodios`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `movie`
+--
+-- Error leyendo la estructura de la tabla peliculas.movie: #1932 - Table &#039;peliculas.movie&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.movie: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`movie`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `portada`
+--
+-- Error leyendo la estructura de la tabla peliculas.portada: #1932 - Table &#039;peliculas.portada&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.portada: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`portada`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `series`
+--
+-- Error leyendo la estructura de la tabla peliculas.series: #1932 - Table &#039;peliculas.series&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.series: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`series`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temporada`
+--
+-- Error leyendo la estructura de la tabla peliculas.temporada: #1932 - Table &#039;peliculas.temporada&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.temporada: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`temporada`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tvshow`
+--
+-- Error leyendo la estructura de la tabla peliculas.tvshow: #1932 - Table &#039;peliculas.tvshow&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.tvshow: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`tvshow`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+-- Error leyendo la estructura de la tabla peliculas.usuarios: #1932 - Table &#039;peliculas.usuarios&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla peliculas.usuarios: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `peliculas`.`usuarios`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
